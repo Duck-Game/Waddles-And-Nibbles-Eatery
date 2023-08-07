27a499e8c227b80aa1c07b16bf80cf39351e0f58
thisischu <chuyilei1@gmail.com>
2023-08-07 19:02:31 +0000

    HomeScreen + GameOver scene added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ kaboom();
 
 //Background image
 loadSprite("background", 'sprites/bg.png');
+loadSprite("home", 'sprites/home.png');
 
 //loading font
 loadFont('arcade', 'ARCADECLASSIC.TTF')
@@ -258,17 +259,83 @@ scene('player1', () => {
 
       //reset duck container
       resetDuckSprites()
-      
+
       //shake on complete order
       shake(2)
-      
+
       //reset timer
       time = 30;
     }
 
-    // if (time === 0) {
-    //   debug.log('lol')
-    // }
+    if (time === 0) {
+      go('gameOver')
+    }
   })
 })
 go('player1')
+
+
+//homescreen
+scene("home", () => {
+  add([
+    sprite('home', { width: width(), height: height() }),
+    scale(1),
+  ])
+
+  function addButton(txt, p, f) {
+
+    // add a parent background object
+    const btn = add([
+      rect(240, 80, { radius: 8 }),
+      pos(p),
+      area(),
+      scale(1),
+      anchor("center"),
+      outline(4)
+    ])
+
+    // add a child object that displays the text
+    btn.add([
+      text(txt),
+      anchor("center"),
+      color(0, 0, 0),
+    ])
+
+    // onHoverUpdate() comes from area() component
+    // it runs every frame when the object is being hovered
+    btn.onHoverUpdate(() => {
+      const t = time() * 10
+      btn.color = hsl2rgb((t / 10) % 1, 0.6, 0.7)
+      btn.scale = vec2(1.2)
+      setCursor("pointer")
+    })
+
+    // onHoverEnd() comes from area() component
+    // it runs once when the object stopped being hovered
+    btn.onHoverEnd(() => {
+      btn.scale = vec2(1)
+      btn.color = rgb()
+    })
+
+    // onClick() comes from area() component
+    // it runs once when the object is clicked
+    btn.onClick(f)
+
+    return btn
+
+  }
+
+  addButton("1 Player", vec2(width() / 2, (height() / 2) - 75), () => go("player1"))
+  addButton("2 Players", vec2(width() / 2, (height() / 2) + 75), () => go("player2"))
+})
+
+
+scene("gameOver", () => {
+  add([
+    text('game over'),
+    anchor("center"),
+    color(0, 0, 0),
+  ])
+})
+
+go('home')
